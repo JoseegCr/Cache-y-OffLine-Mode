688e10c011fed167558e508556607ef0e3bb35c0
JoseegCr <joseegccr@gmail.com>
2022-10-29 10:17:45 -0600

    Guardar APP SHELL

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,26 +1,15 @@
-self.addEventListener('fetch', event => {
+self.addEventListener('install', e => {
+    const cacheProm = caches.open('cache-1').then(cache => {
+        cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            '/js/app.js'
+        ]);
+    });
 
-     /* const offLineResp = new Response(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Mi PWA</title>
-    
-       <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css">-->
-    </head>
-    <body class="container p-3">
-    
-    `);*/
-
-    const offlineResp = fetch ('pages/offline.html');
-
-    const resp = fetch(event.request).catch( () => offLineResp);
-
-    event.respondWith(resp);
+    e.waitUntil(cacheProm);
 });
 
 
